801d949f5fb47c37a0601ba4837b625480c57136
Rohit Jain <rohitjain@TMDCIN095.local>
2024-08-08 17:02:44 +0530

    changes

--- apis/salesbyevent/getsalesbyevent.sql
+++ apis/salesbyevent/getsalesbyevent.sql
@@ -1,11 +1,11 @@
-
+{% cache %} 
 {% req eventidcount %}
     SELECT COUNT(*) as count FROM sales WHERE eventid = {{context.params.id}};
 {% endreq %}
 
 {% if eventidcount.value()[0].count == 0 %}
-    {% error "STORE_NOT_FOUND" %}
+    {% error "EVENT_NOT_FOUND" %}
 {% endif %}
-{% cache %} 
+
     SELECT * FROM sales1_cache WHERE eventid = {{ context.params.id }};
 {% endcache %}    
