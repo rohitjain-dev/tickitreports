9af0786029f8be1a903302d41b59dcd824ddaae2
Rohit Jain <rohitjain@TMDCIN095.local>
2024-08-08 17:20:59 +0530

    changes

--- apis/getsalesbyevent.sql
+++ apis/getsalesbyevent.sql
@@ -1,13 +1,3 @@
-
-{% req eventidcount %}
-    SELECT COUNT(*) as count FROM sales WHERE eventid = {{context.params.id}};
-{% endreq %}
-
-{% cache %} 
-{%if eventidcount.value()[0].count == 0 %}
-    {% error "EVENT_NOT_FOUND" %}
-{%else %}
- SELECT * FROM sales_cache WHERE eventid = {{ context.params.id }};    
-{% endif %}
-   
+{% cache %}
+ SELECT * FROM sales1_cache WHERE eventid = {{ context.params.id }};       
 {% endcache %}    
