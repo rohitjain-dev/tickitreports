0f3a19344304df5f86bbabe88f2a5e6a486c33c2
Rohit Jain <rohitjain@TMDCIN095.local>
2024-09-06 15:17:54 +0530

    Revert "added venue performance"
    This reverts commit 66765ede4684cee6b475a46ffcd41186a916fb7e.

--- apis/getsalesbyevent.sql
+++ apis/getsalesbyevent.sql
@@ -1,3 +1,2 @@
-{% cache %}
-SELECT * FROM sales1 WHERE eventid = {{ context.params.id }};       
-{% endcache %}
+
+ SELECT * FROM sales WHERE eventid = {{ context.params.id }};       
--- apis/venueperformance.sql
+++ apis/venueperformance.sql
@@ -1,18 +1,5 @@
-{%cache%}
-SELECT 
-    venuecache.venueid, 
-    venuecache.venuename, 
-    COUNT(event4.eventid) AS total_events_hosted,
-    SUM(sales4.qtysold) AS total_tickets_sold,
-    SUM(sales4.pricepaid) AS total_revenue
-FROM 
-    venuecache
-JOIN 
-    event4 ON venuecache.venueid = event4.venueid
-JOIN 
-    sales4 ON event4.eventid = sales4.eventid
-GROUP BY 
-    venuecache.venueid, venuecache.venuename
-ORDER BY 
-    total_tickets_sold DESC, total_events_hosted DESC
-{%endcache%}
+SELECT event.venueid, COUNT(event.eventid) AS total_events, SUM(sales.qtysold) AS total_tickets
+FROM public.event
+JOIN public.sales ON event.eventid = sales.eventid
+WHERE event.venueid = {{context.params.id}}
+GROUP BY event.venueid;
