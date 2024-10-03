f889c9f5d5c78ae899ec27a4360afd7b6fc2e08a
Rohit Jain <rohitjain@TMDCIN095.local>
2024-10-03 14:30:22 +0530

    added all venues

--- apis/allvenues.sql
+++ apis/allvenues.sql
@@ -1,9 +1,11 @@
 SELECT 
-    v.venuename, 
-    e.eventname
+    v.venuename,
+    LISTAGG(e.eventname, ', ') WITHIN GROUP (ORDER BY e.eventname) AS eventhosted
 FROM 
     venue v
 JOIN 
     event e ON v.venueid = e.venueid
+GROUP BY 
+    v.venuename
 ORDER BY 
-    v.venuename, e.eventname;
+    v.venuename;
