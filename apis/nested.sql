34ac608ff2aa4f1daa9ab862eed857a7a11df072
Rohit Jain <rohitjain@TMDCIN095.local>
2024-10-03 16:20:59 +0530

    added nested

--- apis/nested.sql
+++ apis/nested.sql
@@ -1,7 +1,7 @@
 SELECT
     public.venue.venuename,
     public.venue.venuecity,
-    public.users.username AS buyer,
+    public.users.username AS buyer
 FROM public.event
 JOIN public.venue ON public.event.venueid = public.venue.venueid
 JOIN public.sales ON public.event.eventid = public.sales.eventid
